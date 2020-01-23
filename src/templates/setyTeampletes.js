604dd9c5581636b27d454ff77d23b1c426864e00
Vitalik Nesterov <vitalistarkiii@gmail.com>
2020-01-24 01:38:24 +0300

    fixedddd

--- src/templates/setyTeampletes.js
+++ src/templates/setyTeampletes.js
@@ -3,16 +3,16 @@ import { graphql } from 'gatsby';
 import SetyItem from '../components/SetyItem';
 
 const SetyTeamplate = ({
-    data: {contentfulProduct}, pageContext}) => { 
+    data: {contentfulProduct}}) => { 
 
 
  return  (
      <>
     <SetyItem 
-        name={pageContext.name}
-        price={pageContext.price}
-        description={pageContext.description}
-        createdAt={pageContext.createdAt}
+        name={contentfulProduct.name}
+        price={contentfulProduct.price}
+        description={contentfulProduct.description}
+        createdAt={contentfulProduct.createdAt}
         image={contentfulProduct.image.fluid}
     > </SetyItem>
     </>
@@ -23,6 +23,10 @@ export default SetyTeamplate
 export const query = graphql ` 
     query ($slug: String!) {
        contentfulProduct(slug: {eq: $slug}) {
+          name
+          price
+          description
+          createdAt(formatString: "МММ Do, YYYY, h:mm:ss a")
           image {
               fluid(maxWidth: 400) {
                   ...GatsbyContentfulFluid
@@ -31,23 +35,16 @@ export const query = graphql `
         }
     }
   `
-
-
-
-// export const query = graphql ` 
-//     query ($slug: String!) {
-//        contentfulProduct(slug: {eq: $slug}) {
-//           name
-//           price
-//           description
-//           createdAt(formatString: "МММ Do, YYYY, h:mm:ss a")
-//           image {
-//               fluid(maxWidth: 400) {
-//                   ...GatsbyContentfulFluid
-//                 }
-//             }
-//         }
-//     }
-//   `
  
 
+//  export const query = graphql ` 
+//  query ($slug: String!) {
+//     contentfulProduct(slug: {eq: $slug}) {
+//        image {
+//            fluid(maxWidth: 400) {
+//                ...GatsbyContentfulFluid
+//              }
+//          }
+//      }
+//  }
+`
