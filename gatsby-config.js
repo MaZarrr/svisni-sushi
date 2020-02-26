534d6e94e6b32bb7326843265677c046a14a6799
Vitalik Nesterov <vitalistarkiii@gmail.com>
2020-02-26 15:15:55 +0300

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,48 +101,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-offline`,
       options: {
-        precachePages: [`/*`],
-        workboxConfig: {
-          importWorkboxFrom: `local`,
-          globDirectory: '/',
-          globPatterns: [
-            '**\/*.{js,css,html,png,jpg,jpeg}',
-          ],
-          // modifyURLPrefix: {
-            // If `pathPrefix` is configured by user, we should replace
-            // the default prefix with `pathPrefix`.
-          //   "/": `${pathPrefix}/`,
-          // },
-          cacheId: `gatsby-plugin-offline`,
-          // Don't cache-bust JS or CSS files, and anything in the static directory,
-          // since these files have unique URLs and their contents will never change
-          dontCacheBustURLsMatching: /(\.js$|\.css$|static\/)/,
-          runtimeCaching: [{
-              // Use cacheFirst since these don't need to be revalidated (same RegExp
-              // and same reason as above)
-              urlPattern: /(\.js$|\.css$|static\/)/,
-              handler: `CacheFirst`,
-            },
-            {
-              // page-data.json files are not content hashed
-              urlPattern: /^https?:.*\page-data\/.*\/page-data\.json/,
-              handler: `NetworkFirst`,
-            },
-            {
-              // Add runtime caching of various other page resources
-              urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
-              handler: `StaleWhileRevalidate`,
-            },
-            {
-              // Google Fonts CSS (doesn't end in .css so we need to specify it)
-              urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
-              handler: `StaleWhileRevalidate`,
-            },
-          ],
-          skipWaiting: true,
-          clientsClaim: true,
-        }
-      },
+        precachePages: [`/*`]
+      }
     },
   ],
 }
