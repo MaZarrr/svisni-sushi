31aee084f9b5313f1207e9cea5553046f3826cab
Vitalik Nesterov <vitalistarkiii@gmail.com>
2020-03-01 15:10:54 +0300

    stress

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -1,15 +1,12 @@
 import React from "react"
 import { Provider } from "react-redux"
 import store from "./src/state/createStore"
-import ErrorBoundary from './src/components/error-boundary/error-boundary';
 
 export default ({ element }) => {
 
   return (
   <Provider store={store}>
-  <ErrorBoundary>
       {element}
-  </ErrorBoundary>
   </Provider>
   )
 }
