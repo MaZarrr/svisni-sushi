401933681119ee9778c8ec9d4722e0f5efcb59f0
Vitalik Nesterov <vitalistarkiii@gmail.com>
2020-07-24 01:26:08 +0300

    sssm

--- src/components/common/CarouselSvisni.js
+++ src/components/common/CarouselSvisni.js
@@ -3,24 +3,44 @@ import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
 import { graphql, useStaticQuery, Link } from "gatsby"
 import Img from 'gatsby-image';
+import Paper from "@material-ui/core/Paper";
+import {makeStyles} from '@material-ui/core/styles';
+import Typography from "@material-ui/core/Typography";
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
-const styles = {
+const useStyleCarousel = makeStyles(theme => ({
     root: {
-        padding: '0 30px',
+        maxWidth: `100vw`,
+        flexGrow: '1',
+        [theme.breakpoints.down('768')]: {
+            marginBottom: 40,
+        },
+        [theme.breakpoints.down('475')]: {
+            marginBottom: 0,
+        },
     },
-    slideContainer: {
-        padding: '0 10px',
+    header: {
+        paddingTop: '8px',
+        background: `#f0ecec`,
     },
-    slide: {
-        padding: 15,
-        minHeight: 100,
-        color: '#fff',
+    h1Home: {
+        fontFamily: 'Oswald, cursive',
+        fontWeight: '800',
+        lineHeight: 2,
+        fontSize: '46px',
+        padding: `0 0 0 35px`,
+        [theme.breakpoints.down('600')]: {
+            fontSize: '32px',
+            color: `#000`,
+            textAlign: `center`,
+            padding: 0
+        },
+    },
+    button: {
+        margin: `0 auto`,
+        width: `100%`
     }
-};
-
-function DemoWidth() {
-
+}));
 
 const styles = {
     root: {
@@ -51,6 +71,11 @@ function DemoWidth() {
                                 ...GatsbyContentfulFluid
                             }
                         }
+                        imgCarouselPhone {
+                            fluid(maxWidth: 400, quality: 90) {
+                                ...GatsbyContentfulFluid
+                            }
+                        }
                     }
                 }
             }
@@ -58,17 +83,21 @@ function DemoWidth() {
     `)
 
     return (
-        <AutoPlaySwipeableViews style={styles.root} slideStyle={styles.slideContainer}>
-            {data.allContentfulCarouselSiteImage.edges.map((step, index) => (
-                      <div key={step.node.id}>
-                                 <Link to={step.node.slug}>
-                                     <Img fluid={step.node.imgCarouselPc.fluid} style={{borderRadius: 13, marginTop: 5}} imgStyle={{maxWidth: 1300}} alt={step.node.id} />
-                                 </Link>
-                         </div>
-                     ))}
-        </AutoPlaySwipeableViews>
-    );
+        <div className={classes.root}>
+            <Paper square elevation={0} className={classes.header}>
+                <Typography variant="h1" className={classes.h1Home}>Свисни Суши в Уразово</Typography>
+            </Paper>
+            <AutoPlaySwipeableViews style={styles.root} slideStyle={styles.slideContainer}>
+                {data.allContentfulCarouselSiteImage.edges.map((step, index) => (
+                    <div key={step.node.id}>
+                        <Link to={step.node.slug}>
+                            <Img fluid={step.node.imgCarouselPc.fluid} style={{borderRadius: 13, marginTop: 5}} imgStyle={{maxWidth: 1300}} alt={step.node.id} />
+                        </Link>
+                    </div>
+                ))}
+            </AutoPlaySwipeableViews>
+        </div>
+    )
 }
 
 export default DemoWidth;
-
