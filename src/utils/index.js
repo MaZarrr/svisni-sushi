b23c8ea0ed7827a2a82987096faf7ad7dc1c3552
MaZarrr <vitalistarkiii@gmail.com>
2024-07-20 23:14:34 +0300

    add utils getFormattedDate

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,6 +4,18 @@ export function hasEntries(object) {
     return Object.entries(object).length > 0;
   }
 
+export function getFormattedDate() {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+  const day = String(currentDate.getDate()).padStart(2, '0');
+  const hours = String(currentDate.getHours()).padStart(2, '0');
+  const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+
+  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}`;
+  return formattedDate;
+}
+
 export const sendRequest = async () => {
     try {
       const res = await fetch(`https://svisniplatform.site/getWall`, {
