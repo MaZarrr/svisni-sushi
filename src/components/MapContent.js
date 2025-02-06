8d0ea896c8697cbcd9e41df869692e4ad31715de
MaZarrr <vitalistarkiii@gmail.com>
2025-02-06 13:11:57 +0300

    switsh map to address

--- src/components/MapContent.js
+++ src/components/MapContent.js
@@ -1,31 +1,76 @@
-import React from 'react'
-import { Map, Placemark } from "react-yandex-maps"
+import React, { useEffect, useState } from 'react'
+import { Map, Placemark, RoutePanel } from '@pbe/react-yandex-maps';
 import styled from '@emotion/styled'
 
-const MapContainer = styled.div`
-        box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2);
-        width: 90%;
-        margin: 20px auto 50px auto;
-        .mapStyle {
-            width: 100%;
-            height: 700px;
-            padding: 30px;
-            margin: auto;
-            @media (max-width: 768px) {
-              height: 400px;
-              padding: 10px;
-        }
-    }
-`
+const MapContainer = styled("div")(({ theme }) => ({
+  boxShadow: "0px 5px 10px 2px rgba(34, 60, 80, 0.2)",
+  width: "90%",
+  margin: "20px auto 50px auto",
+  padding: "30px",
 
-const MapContent = () => (
+  "& .mapStyle": { 
+    width: "100%",
+    height: "700px",
+    padding: "30px",
+    margin: "auto",
+
+    [theme.breakpoints.down("md")]: {
+      height: "400px",
+      padding: "10px",
+    },
+  },
+
+  [theme.breakpoints.down("sm")]: {
+    padding: 0,
+    margin: 0,
+    width: '100%'
+  },
+}));
+
+
+const MapContent = ({ value }) => {
+ 
+  const [mapState, setMapState] = useState({
+      center: [55.751574, 37.573856], // Начальные координаты (Москва)
+      zoom: 16.5,
+      controls: ["zoomControl", "fullscreenControl"],
+      behaviors: ["drag", "dblClickZoom", "multiTouch"],
+    });
+
+    const [placemarkCoords, setPlacemarkCoords] = useState([50.081738, 38.025075]); // Начальные координаты
+
+    const movePlacemark = ({ lat, son }) => {
+      setPlacemarkCoords([lat, son]); 
+    };
+
+  const updateCenter = ({ lat, son }) => {
+    setMapState((prevState) => ({
+      ...prevState,
+      center: [lat, son],
+    }));
+  };
+
+    useEffect(() => {
+
+      if(value === 0) {
+        updateCenter({ lat: 50.219224, son: 38.097911  })
+        movePlacemark({  lat: 50.219224, son: 38.097911})
+      } else {
+        updateCenter({ lat: 50.081738, son: 38.025075 })
+        movePlacemark({ lat: 50.081738, son: 38.025075})
+      }
+    }, [value])
+
+ 
+    return (
     <MapContainer>
       <Map
-        defaultState={{
-          center: [50.081738, 38.025075],
-          zoom: 16.5,
-          controls: ['zoomControl', 'fullscreenControl'],
-          behaviors: ['drag', 'dblClickZoom', 'multiTouch'] }}
+        state={mapState} 
+        // defaultState={{
+        //   center: [coords.lat, coords.son],
+        //   zoom: 16.5,
+        //   controls: ['zoomControl', 'fullscreenControl'],
+        //   behaviors: ['drag', 'dblClickZoom', 'multiTouch'] }}
         modules={['control.ZoomControl', 'control.FullscreenControl']}
         className="mapStyle"
         properties={{
@@ -33,15 +78,18 @@ const MapContent = () => (
             'Суши бар Свисни Суши в Уразово',
         }}>
         <Placemark
-          defaultGeometry={[50.081738, 38.025075]}
+          // defaultGeometry={[coords.lat, coords.son]}
+          geometry={placemarkCoords}
           modules={["geoObject.addon.balloon"]}
           properties={{
             balloonContentHeader: "Свисни Суши",
             balloonContent: 'ул.Красная Площадь 30А, Уразово'
           }}
         />
+          {/* <RoutePanel options={{ float: "right" }} /> */}
       </Map>
     </MapContainer>
   )
+        }
 
 export default MapContent
